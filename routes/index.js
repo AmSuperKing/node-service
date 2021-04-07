5f30a647852816ff0e6dfdcf983b44a92fe31386
wangjian <wangjian5092@163.com>
2021-04-07 22:20:07 +0800

    some change

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Index' });
+  res.send('Welcome to Index page.');
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('This is get /user respond to you.');
+});
+router.get('/login', function(req, res, next) {
+  res.send('This is get /user/login respond to you.');
 });
 
 module.exports = router;
